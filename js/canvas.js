9071c26b3a0c2409eb5250f376d9e5cb0af2f4e0
Eric Julius <ejulius86@gmail.com>
2016-09-06 03:19:19 -0500

    Cleaning up canvas functions for move to 3D

--- js/canvas.js
+++ js/canvas.js
@@ -1,27 +1,16 @@
 /* jshint esversion:6 */
-let canvasWidth, canvasHeight, drawScale, choke;
-let canvasDiv = document.getElementById("canvas-shell");
+let canvasWidth, canvasHeight, drawScale, chokecanvasData;
 let canvas = document.createElement("canvas");
 let ctx = canvas.getContext("2d");
-let canvasData;
-
-canvas.addEventListener('mousemove', function(evt) {
-	let x = (evt.clientX - canvas.offsetLeft) / drawScale;
-	let y = (evt.clientY - canvas.offsetTop) / drawScale;
-  var pos = x + ', ' + y;
-	var rawRGB = ctx.getImageData(x, y, 1, 1).data;
-	let message = pos + ' rgb(' + rawRGB[0] + ', ' + rawRGB[1] + ', ' + rawRGB[2] + ')';
-  document.getElementById('cursor-pos').innerHTML = message;
-}, false);
 
 // Monochrome
-function frameDraw(frame) {
+function drawMonoArray(frame) {
 	let index;
 
 	for (let x = 0; x < gridX; x++) {
 		for (let y = 0; y < gridY; y++) {
 			index = (x + y * canvasWidth) * 4;
-			let pixel = Math.abs(frame[x][y]) / 2.0 * 255 & 255;
+			let pixel = Math.max(frame[x][y] * 255, 0);
 
 			canvasData.data[index + 0] = pixel;
 			canvasData.data[index + 1] = pixel;
@@ -31,8 +20,7 @@ function frameDraw(frame) {
 	}
 }
 
-// RGB
-function rgbPixelDraw(x, y, rgb) {
+function drawRgbPixel(x, y, rgb) {
 	let index = (x + y * canvasWidth) * 4;
 	canvasData.data[index + 0] = rgb[0];
 	canvasData.data[index + 1] = rgb[1];
@@ -40,17 +28,15 @@ function rgbPixelDraw(x, y, rgb) {
 	canvasData.data[index + 3] = 255;
 }
 
-
-
 let updateCanvas = function() {
 	ctx.putImageData(canvasData, 0,0);
 };
 
-let setStartingValues = function() {
-	canvasWidth = document.getElementById("width").value || 512;
-	canvasHeight = document.getElementById("height").value || 512;
-	drawScale = document.getElementById("scale").value || 1;
-	frequency = document.getElementById("frequency").value || 0.0125;
+function setInitValues() {
+	canvasWidth = $("#width").val() || 512;
+	canvasHeight = $("#height").val() || 512;
+	drawScale = $("#scale").val() || 1;
+	frequency = $("#frequency").val() || 0.0125;
 	canvasData = ctx.getImageData(0,0,canvasWidth, canvasHeight);
 
 	canvas.setAttribute("width", canvasWidth);
@@ -58,12 +44,16 @@ let setStartingValues = function() {
 	canvas.setAttribute("id", "canvas");
 	canvas.setAttribute("style", "border-style:solid; border-width: 1px; image-rendering: pixelated;" +
 		"width:" + Math.round(canvasWidth*drawScale) + "px; height:" + Math.round(canvasHeight*drawScale) + "px;");
-	canvasDiv.appendChild(canvas);
+	$("#canvas-shell").append(canvas);
 
 	gridY = canvasHeight;
 	gridX = canvasWidth;
-};
-
-window.onload = function(){
-	startGen();
-};
+	canvas.addEventListener('mousemove', function(evt) {
+		let x = (evt.clientX - canvas.offsetLeft) / drawScale;
+		let y = (evt.clientY - canvas.offsetTop) / drawScale;
+	  var pos = x + ', ' + y;
+		var rawRGB = ctx.getImageData(x, y, 1, 1).data;
+		let message = pos + ' rgb(' + rawRGB[0] + ', ' + rawRGB[1] + ', ' + rawRGB[2] + ')';
+	  document.getElementById('cursor-pos').innerHTML = message;
+	}, false);
+}
