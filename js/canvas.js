280965aeb4df12a69689fb70d6e8f429c77aabf4
Eric Julius <ejulius86@gmail.com>
2016-09-15 00:55:38 -0500

    Cleaned up stuff based on eslint recommendations. Updated the cursor position text.

--- js/canvas.js
+++ js/canvas.js
@@ -1,32 +1,35 @@
 /* jshint esversion:6 */
 let resolutionScale, canvasData;
-let canvas = $("<canvas></canvas>").attr("id", "canvas");
+let canvas = $('<canvas></canvas>').attr('id', 'canvas');
 let ctx = canvas.get(0).getContext('2d');
 
 function setupCanvas() {
-  resolutionScale = $("#scale").val() || 1;
+  resolutionScale = $('#scale').val() || 1;
   canvas.prop({
-    width: $("#width").val(),
-    height: $("#height").val()
+    width: $('#width').val(),
+    height: $('#height').val()
   }).css({
-    "border-style": "solid",
-    "border-width": "1px",
-    "image-rendering": "pixelated"
+    'border-style': 'solid',
+    'border-width': '1px',
+    'image-rendering': 'pixelated'
   });
   canvas
-    .width($("#width").val() * resolutionScale)
-    .height($("#height").val() * resolutionScale);
+    .width($('#width').val() * resolutionScale)
+    .height($('#height').val() * resolutionScale);
   canvasData = ctx.getImageData(0, 0, canvas.get(0).width, canvas.get(0).height);
-  $("#canvas-shell").append(canvas);
+  $('#canvas-shell').append(canvas);
 }
 
-canvas.mousemove(function (evt) {
-  let x = evt.offsetX;
-  let y = evt.offsetY;
-  var pos = x + ', ' + y;
-  var rawRGB = ctx.getImageData(x / resolutionScale, y / resolutionScale, 1, 1).data;
-  let message = pos + ' rgb(' + rawRGB[0] + ', ' + rawRGB[1] + ', ' + rawRGB[2] + ')';
-  $("#cursor-pos").text(message);
+canvas.mousemove((evt) => {
+  const x = Math.round(evt.offsetX / resolutionScale);
+  const y = Math.round(evt.offsetY / resolutionScale);
+  const pos = `(${x}, ${y})`;
+  // const rawRGB = ctx.getImageData(x / resolutionScale, y / resolutionScale, 1, 1).data;
+  const message = `${pos}<br/>e: ${mapGen.maps.height.map[x][y]}
+    <br/>st: ${mapGen.maps.subTemperature.map[x][y]}
+    <br/>m: ${mapGen.maps.moisture.map[x][y]}
+    <br/>b: ${mapGen.maps.biome.map[x][y][3]}`;
+  $('#cursor-pos').html(message);
 });
 
 
@@ -35,7 +38,7 @@ function drawMonoArray(frame) {
   'use strict';
   for (let x = 0; x < frame.length; x++) {
     for (let y = 0; y < frame[x].length; y++) {
-      let index = (x + y * canvas.get(0).width) * 4;
+      const index = (x + y * canvas.get(0).width) * 4;
       let pixel = frame[x][y] * 255;
       if (pixel > 255) pixel = 255;
       for (let i = 0; i < 3; i++) {
