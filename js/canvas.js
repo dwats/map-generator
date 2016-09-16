aeed651342a1829b4d9159208a96bea442be8b8f
Eric Julius <ejulius86@gmail.com>
2016-09-16 03:41:32 -0500

    Removed prototype OpenGl rendering code.

--- js/canvas.js
+++ js/canvas.js
@@ -253,75 +253,6 @@ function Create3dObject(settings) { // eslint-disable-line no-unused-vars
   return create3dObject;
 }
 
-function draw3d() { // eslint-disable-line no-unused-vars
-  // TODO refactor this.
-  scene = new THREE.Scene();
-  const screenWidth = 512;
-  const screenHeight = 512;
-  const viewAngle = 75;
-  const aspect = screenWidth / screenHeight;
-  const near = 0.1;
-  const far = 20000;
-  camera = new THREE.PerspectiveCamera(viewAngle, aspect, near, far);
-  camera.position.z = 1000;
-  camera.position.y = 240;
-  camera.position.x = 0;
-
-  renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
-  renderer.setSize(screenWidth, screenHeight);
-  controls = new THREE.OrbitControls(camera, renderer.domElement);
-
-  camera.lookAt(new THREE.Vector3(0, 0, 0));
-  container = $('#canvas-shell-3d');
-  container.html(renderer.domElement);
-
-  // Geometry Ground
-  geoGround = new THREE.PlaneGeometry(1024, 1024, 255, 255);
-  const matGround = new THREE.MeshLambertMaterial({
-    color: 0xccccff,
-    wireframe: false
-  });
-  const terrain = getTerrainFromCanvas();
-  for (let i = 0, l = geoGround.vertices.length; i < l; i++) {
-    const terrainValue = terrain[i] / 255;
-    geoGround.vertices[i].z += (terrainValue * 255);
-  }
-  geoGround.computeFaceNormals();
-  geoGround.computeVertexNormals();
-
-  // Geometry Water
-  const geoWater = new THREE.PlaneGeometry(1000, 1000, 11, 11);
-  const matWater = new THREE.MeshLambertMaterial({
-    color: 0x00ccff,
-    wireframe: false,
-    transparent: true,
-    opacity: 0.7
-  });
-
-  // Plane Ground
-  const planeGround = new THREE.Mesh(geoGround, matGround);
-  planeGround.position = new THREE.Vector3(0, 0, 0);
-  const q = new THREE.Quaternion();
-  q.setFromAxisAngle(new THREE.Vector3(-1, 0, 0), (90 * Math.PI) / 180);
-  planeGround.quaternion.multiplyQuaternions(q, planeGround.quaternion);
-  scene.add(planeGround);
-
-  // Plane Water
-  const planeWater = new THREE.Mesh(geoWater, matWater);
-  planeWater.position = new THREE.Vector3(0, 0, 0);
-  planeWater.rotateX(-Math.PI / 2);
-  planeWater.position.y = 76;
-  scene.add(planeWater);
-
-  addLights();
-  render();
-}
-
-function render() {
-  requestAnimationFrame(render);
-  renderer.render(scene, camera);
-}
-
 function getTerrainFromCanvas() {
   const data = ctx.getImageData(0, 0, canvas.get(0).width, canvas.get(0).width).data;
   const normPixels = [];
@@ -331,15 +262,4 @@ function getTerrainFromCanvas() {
   }
 
   return normPixels;
-}
-
-function addLights() {
-  const ambientLight = new THREE.AmbientLight(0x444444);
-  const directionalLight = new THREE.DirectionalLight(0xffffff);
-
-  ambientLight.intensity = 0.0;
-  directionalLight.position.set(900, 400, 0).normalize();
-
-  scene.add(ambientLight);
-  scene.add(directionalLight);
 }
